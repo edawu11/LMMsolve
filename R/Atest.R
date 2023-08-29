0018fdbe2ada2e5ac8a4dd9f8e80bc3f8fa4d483
edawu11 <edawu1736@gmail.com>
2023-08-29 14:17:36 +0800

    modify Atest

--- R/Atest.R
+++ R/Atest.R
@@ -9,6 +9,6 @@ Atest = function(thetahat,theta0,S){
   k = length(thetahat)
   S_inv = solve(S)
   statistic = t(thetahat-theta0) %*% S_inv %*% (thetahat-theta0)
-  p_value = pchisq(q=as.numeric(statistic),df =k,lower.tail = TRUE)
+  p_value = pchisq(q=as.numeric(statistic),df =k,lower.tail = FALSE)
   return(p_value)
 }
