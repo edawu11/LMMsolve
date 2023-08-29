f3025574a1169164814541cbe8b86644290555f2
edawu11 <edawu1736@gmail.com>
2023-08-29 12:49:17 +0800

    update

--- R/LMM_MM.R
+++ R/LMM_MM.R
@@ -3,35 +3,34 @@
 ##y:n*1 vector
 ##X:n*p design matrix
 ##Z:n*c covariate matrix
-##ifeff: if use the unbiased estimator of the trace of K^2
-##B:if ifeff is TURE, then set B (e.g. B=100)
+##maxiter: the max iter
+##tol: e.g. 1e-6
+##sb2: initial value of sb2
+##se2: initial value of se2
+##ifintercept: if add 1 vector to Z
+##ifed: if use Eigen decomposition
+##ifmargi: if compute marginal likelihood
 #####output####
-##estimator: a list:sb2,se2,omega
+##estimator: a list:sb2,se2,omega,allL,allsb2,allse2,cov_sig2,h2,cov_h2
 
-LMM_MM = function(y,X,Z,maxiter,tol=1e-6,sb2,se2,ifintercept=F,ifed=F,seed=2023){
+LMM_MM = function(y,X,Z,maxiter,tol=1e-6,sb2,se2,ifintercept=F,ifed=T,ifmargi=F){
   X = as.matrix(X)
   Z = as.matrix(Z)
   
-  set.seed(seed)
   p = ncol(X)
   n = length(y)
   
+  if(ifintercept==T){Z = cbind(1,Z)}
   
-  if(ifintercept==F){
-    q = ncol(Z)
-  }
-  else{
-    Z = cbind(1,Z)
-    q = ncol(Z)
-  }
   y = scale(y,center = T,scale = F) # y is centered, however, no difference?
   X = scale(X,center = T,scale = F) # each column of X is centered
   Xsd = sqrt(colMeans(X^2))
   X = t(t(X)/Xsd)/sqrt(p)
   
   K = X%*%t(X)
-  V = diag(n) - Z%*%solve(t(Z)%*%Z)%*%t(Z)
+
   
+  #store
   allL = c()
   allsb2 = sb2
   allse2 = se2
@@ -69,20 +68,28 @@ LMM_MM = function(y,X,Z,maxiter,tol=1e-6,sb2,se2,ifintercept=F,ifed=F,seed=2023)
     #     }
     #   }
     # }
+    
     eigenK = eigen(K)
     d = eigenK$values #vector
     U = eigenK$vectors #matrix
-    
     UtZ = t(U)%*%Z
     Uty = t(U)%*%y
+    
+    #initial value
+    omega = solve(t(UtZ)%*%UtZ,t(UtZ)%*%Uty)
+    res = y-Z%*%omega
+    sb2 = ifelse(is.null(sb2),drop(var(res)/2),sb2)
+    se2 = ifelse(is.null(se2),drop(var(res)/2),se2)
     invd = 1/(d*sb2+se2)
     
     for(t in 1:maxiter){
       
-      tmp = t(UtZ*invd)
-      # omega = solve(tmp%*%UZ) %*% tmp%*%Uty
-      omega = solve(tmp%*%UtZ,tmp%*%Uty) 
-      res = Uty-UtZ%*%omega
+      if(ifmargi==F){
+        tmp = t(UtZ*invd)
+        # omega = solve(tmp%*%UZ) %*% tmp%*%Uty
+        omega = solve(tmp%*%UtZ,tmp%*%Uty)
+        res = Uty-UtZ%*%omega
+      }
       
       sb2 = sb2*sqrt(sum(res^2*invd^2*d)/sum(invd*d))
       allsb2 = c(allsb2,sb2)
@@ -91,9 +98,9 @@ LMM_MM = function(y,X,Z,maxiter,tol=1e-6,sb2,se2,ifintercept=F,ifed=F,seed=2023)
       allse2 = c(allse2,se2)
       
       invd = 1/(d*sb2+se2)
-      L_sig = 0.5*sum(log(invd)) - 0.5 *sum(res^2*invd)- n/2*log(2*pi)
+      L = 0.5*sum(log(invd)) - 0.5 *sum(res^2*invd)
       
-      allL = c(allL,L_sig)
+      allL = c(allL,L)
       if(t>1){
         if(abs(allL[t] - allL[t-1]) < tol){
           break
@@ -123,12 +130,12 @@ LMM_MM = function(y,X,Z,maxiter,tol=1e-6,sb2,se2,ifintercept=F,ifed=F,seed=2023)
       Sig = sb2*K+se2*diag(n)
       invSig = solve(Sig)
       ZinvSig = t(Z) %*% invSig
-      L_sig = 0.5*log(det(Sig)) - 0.5 *t(res) %*%invSig %*%res- n/2*log(2*pi)
+      L_sig = 0.5*log(det(Sig)) - 0.5 *t(res) %*%invSig %*%res
       
       allL = c(allL,L_sig)
       
       if(t>1){
-        if((allL[t] - allL[t-1]) < tol){
+        if(abs(allL[t] - allL[t-1]) < tol){
           break
         }
       }
@@ -143,17 +150,16 @@ LMM_MM = function(y,X,Z,maxiter,tol=1e-6,sb2,se2,ifintercept=F,ifed=F,seed=2023)
   FIM[2,2] = sum(invSig^2)/2
   FIM[1,2] = sum(invSig*invSigK)/2
   FIM[2,1] = FIM[1,2]
-  cov_sigma2 = solve(FIM)
+  cov_sig2 = solve(FIM)
   
   #delta method
   sigma2_total = sum(sb2+se2)
   h2 = sb2/sigma2_total
   
   dh2 = c(se2/(sigma2_total^2),-sb2/(sigma2_total^2))
+  cov_h2 = as.numeric(t(dh2)%*%cov_sig2%*%dh2) 
   
-  cov_h2 = as.numeric(t(dh2)%*%cov_sigma2%*%dh2) 
-  
-  return(list(sb2=sb2,se2=se2,allL=allL,allsb2=allsb2,allse2 = allse2,
-              cov_sigma2 = cov_sigma2, cov_h2 = cov_h2))
+  return(list(sb2=sb2,se2=se2,omega =omega,allL=allL,allsb2=allsb2,allse2 = allse2,
+              cov_sig2 = cov_sig2,h2=h2, cov_h2 = cov_h2))
 }
 
--- R/LMM_MoM.R
+++ R/LMM_MoM.R
@@ -3,10 +3,12 @@
 ##y:n*1 vector
 ##X:n*p design matrix
 ##Z:n*c covariate matrix
-##ifeff: if use the unbiased estimator of the trace of K^2
-##B:if ifeff is TURE, then set B (e.g. B=100)
+##ifintercept: if add 1 vector to Z
+##ifK2: if use the unbiased estimator of the trace of K^2
+##B:if ifK2 is FALSE, then set B (e.g. B=100)
+##seed
 #####output####
-##estimator: a list:sb2,se2,omega
+##estimator: a list:sb2,se2,omega,cov_sig2,h2,cov_h2
 
 LMM_MoM = function(y,X,Z,ifintercept=F,ifK2=T,B=100,seed=2023){
   X = as.matrix(X)
@@ -37,8 +39,8 @@ LMM_MoM = function(y,X,Z,ifintercept=F,ifK2=T,B=100,seed=2023){
   trVK = sum(diag(VK))
   
   if(ifK2==T){
-    trVK2 = sum(VK^2)
-    # trVK2 = norm(VK,type = "F")
+    # trVK2 = sum(VK^2)
+    trVK2 = norm(VK,type = "F")
   }
   else{
     Zd = matrix(rnorm(n*B),n,B)
@@ -54,18 +56,18 @@ LMM_MoM = function(y,X,Z,ifintercept=F,ifK2=T,B=100,seed=2023){
   A_inv = solve(A)
   sigma2 = A_inv%*%rc
   
-  sigma2_beta = sigma2[1]
-  sigma2_e = sigma2[2]
+  sb2 = sigma2[1]
+  se2 = sigma2[2]
   
   # solve w, use weighted least square
-  U = sigma2_beta*K + sigma2_e*diag(n)
+  U = sb2*K + se2*diag(n)
   U_inv = solve(U)
   omega = solve(t(Z)%*%U_inv%*%Z)%*%t(Z)%*%U_inv%*%y
   
   # compute the variance matrix of sigma2 using sandwich estimator
   
   cov_B = matrix(0,2,2)
-  VSig = sigma2_beta*VK + sigma2_e*V
+  VSig = sb2*VK + se2*V
   VKVSig = VK%*%VSig
   
   cov_B[1,1] = 2*sum(VKVSig^2)
@@ -73,21 +75,21 @@ LMM_MoM = function(y,X,Z,ifintercept=F,ifK2=T,B=100,seed=2023){
   cov_B[1,2] = 2*sum(diag(VKVSig%*%VSig))
   cov_B[2,1] = cov_B[1,2]
   
-  cov_sigma2 = A_inv%*%cov_B%*%A_inv
+  cov_sig2 = A_inv%*%cov_B%*%A_inv
   
   # compute the variance matrix of h2 using delta method
   
   sigma2_total = sum(sigma2)
-  h2 = sigma2_beta/sigma2_total
+  h2 = sb2/sigma2_total
   
-  dh2 = c(sigma2_e/(sigma2_total^2),-sigma2_beta/(sigma2_total^2))
+  dh2 = c(se2/(sigma2_total^2),-sb2/(sigma2_total^2))
   
-  cov_h2 = as.numeric(t(dh2)%*%cov_sigma2%*%dh2) 
+  cov_h2 = as.numeric(t(dh2)%*%cov_sig2%*%dh2) 
   
-  result = list(sb2 =sigma2_beta,
-                se2 = sigma2_e,
+  result = list(sb2 =sb2,
+                se2 = se2,
                 omega = omega,
-                cov_sigma2 = cov_sigma2,
+                cov_sig2 = cov_sig2,
                 h2 = h2,
                 cov_h2 = cov_h2)
   return(result)
